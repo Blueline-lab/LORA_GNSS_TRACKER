61bf0b6917aa02f3a53d4d8c21a00afc0c46f0ac
Blueline-lab <78507019+Blueline-lab@users.noreply.github.com>
2023-07-24 15:47:37 +0200

    Add files via upload

---
+++ RX/USB_BRIDGE.py
@@ -0,0 +1,29 @@
+from db import Geodata_plotting
+from time import sleep
+Listen = Geodata_plotting()
+from random import uniform
+
+usb = "/dev/ttyACM0"
+table = True
+last_value = ""
+
+
+while True:
+    with open(usb, "r") as file :
+        recp = file.readline()
+        delim = ","
+        value = recp.split(delim)
+
+        if table:
+            Listen.db_if_table_exist()
+            table = False
+        
+        if last_value != value and value[2] != '0.0':
+           Listen.db_write_data(value[2], value[3], value[4])
+           
+        else: print("NO SIGNAL")
+        
+        last_value = value
+        value = ""
+
+    
